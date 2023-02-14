d491ae2a87d7ab49a004e7aba8647d0559ed6fa2
Luis Perez Morales <61613934+lperezmo@users.noreply.github.com>
2023-02-13 19:52:21 -0600

    Updated pdf file link, links, and descriptions

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -6,12 +6,12 @@ const config = {
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: ['Data-stealer'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: '',
+    linkedin: 'luis-perez-morales-763a951b1',
     twitter: '',
     facebook: '',
     instagram: '',
@@ -26,38 +26,35 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1zVOgxefN1YSI3HlFC9Q3yIoT3ni6Aaly/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
+    'Python',
+    'Git',
     'JavaScript',
+    'HTML',
+    'CSS',
+    'Docker',
     'React.js',
     'Node.js',
-    'Nest.js',
     'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Ruby',
+    'Pascal',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Hess Services',
+      position: 'Lead Programmer',
+      from: 'November 2022',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://hess-services.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
+      company: 'University of Oregon Graduate School',
+      position: 'Graduate Employee',
       from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      to: 'December 2021',
+      companyLink: 'https://chemistry.uoregon.edu/graduate/',
     },
   ],
   /* certifications: [
@@ -70,16 +67,16 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Umiversity of Oregon Graduate School',
+      degree: 'Master of Science',
+      from: '2019',
+      to: '2021',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Linfield College',
+      degree: 'Bachelor of Science',
+      from: '2015',
+      to: '2019',
     },
   ],
 
@@ -115,7 +112,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'winter',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -174,12 +171,12 @@ const config = {
     },
   },
 
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
-};
+//   // Optional Footer. Supports plain text or HTML.
+//   footer: `Made with <a 
+//       class="text-primary" href="https://github.com/arifszn/gitprofile"
+//       target="_blank"
+//       rel="noreferrer"
+//     >GitProfile</a> and ❤️`,
+// };
 
 export default config;
